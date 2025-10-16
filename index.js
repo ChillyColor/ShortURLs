44231900175b63c13e3e38a04eaa9324a9f71933
Aakarsh Divyam <divyamaakarsh22@gmail.com>
2025-10-17 00:45:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -243,7 +243,7 @@ passport.use(
   new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000' || process.env.CALLBACK_URL,
+    callbackURL: process.env.CALLBACK_URL,
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
   },
   async(accessToken,refreshToken,profile,cb)=>{
